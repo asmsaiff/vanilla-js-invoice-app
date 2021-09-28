6f406e7f50d0ce37c8e8fc9ab6d2808af43f69ec
saidmsaif <saidmsaiff@gmail.com>
2021-09-29 01:00:07 +0600

    implementation delete method [not completed yet

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const invoice_form = document.getElementById("invoice_form");
-const delete_item = document.querySelectorAll("delete_item");
 
 // Form Data
 const your_name = document.getElementById('your_name')
@@ -36,122 +35,145 @@ let unit = [];
 invoice_form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-    if (your_name.value == '' || item_name.value == '' || qty.value == '' || flat_rate.value == '' || unit_rate.value == '') {
-        alert('Please, fill all input fields!!!')
-    } else {
-        delete_item.addEventListener('click', (e) => {
-            e.preventDefault()
+    if (
+		your_name.value == "" ||
+		item_name.value == "" ||
+		qty.value == "" ||
+		flat_rate.value == "" ||
+		unit_rate.value == ""
+	) {
+		alert("Please, fill all input fields!!!");
+	} else {
+		// const delete_item = document.querySelectorAll("delete_item");
 
-            delete_item.forEach((val, key) => {
-                val.classList.contains('delete')
-            })
-        })
+		// delete_item.addEventListener('click', (e) => {
+		//     e.preventDefault()
 
-        data = {
-            name: your_name.value,
-            address: address.value,
-            city: city.value,
-            item_name: item_name.value,
-            description: description.value,
-            qty: qty.value,
-            flat_rate: flat_rate.value,
-            unit_rate: unit_rate.value,
-        },
+		//     delete_item.forEach((val, key) => {
+		//         val.classList.contains('delete')
+		//     })
+		// })
+
+		data = {
+			name: your_name.value,
+			address: address.value,
+			city: city.value,
+			item_name: item_name.value,
+			description: description.value,
+			qty: qty.value,
+			flat_rate: flat_rate.value,
+			unit_rate: unit_rate.value,
+		},
         form_data.push(data);
 
-        form_data.forEach((val, key) => {
-            show_name.innerHTML = val.name
-            const tr = document.createElement("tr");
+		form_data.forEach((val, key) => {
+			show_name.innerHTML = val.name;
+			const tr = document.createElement("tr");
 
-            const td1 = document.createElement("td");
-            td1.classList = "text-center border";
-            td1.innerHTML = key + 1;
+			const td1 = document.createElement("td");
+			td1.classList = "text-center border";
+			td1.innerHTML = key + 1;
 
-            const td2 = document.createElement("td");
-            td2.classList = "flex flex-col border pl-3";
-            const create_name_span = document.createElement("span");
-            create_name_span.innerHTML = val.item_name;
+			const td2 = document.createElement("td");
+			td2.classList = "flex flex-col border pl-3";
+			const create_name_span = document.createElement("span");
+			create_name_span.innerHTML = val.item_name;
 
-            const create_desc_span = document.createElement("span");
-            create_desc_span.classList = "text-sm text-gray-500";
-            create_desc_span.id = "show_desc";
-            create_desc_span.innerHTML = val.description;
+			const create_desc_span = document.createElement("span");
+			create_desc_span.classList = "text-sm text-gray-500";
+			create_desc_span.id = "show_desc";
+			create_desc_span.innerHTML = val.description;
 
-            td2.appendChild(create_name_span);
-            td2.appendChild(create_desc_span);
+			td2.appendChild(create_name_span);
+			td2.appendChild(create_desc_span);
 
-            const td3 = document.createElement("td");
-            td3.classList = "text-center border";
-            td3.id = "qts";
-            td3.innerHTML = val.qty;
+			const td3 = document.createElement("td");
+			td3.classList = "text-center border";
+			td3.id = "qts";
+			td3.innerHTML = val.qty;
 
-            const td4 = document.createElement("td");
-            td4.classList = "text-center border";
-            td4.innerHTML = val.flat_rate;
+			const td4 = document.createElement("td");
+			td4.classList = "text-center border";
+			td4.innerHTML = val.flat_rate;
 
-            const td5 = document.createElement("td");
-            td5.classList = "text-center border";
-            td5.innerHTML = val.unit_rate;
+			const td5 = document.createElement("td");
+			td5.classList = "text-center border";
+			td5.innerHTML = val.unit_rate;
 
-            const td6 = document.createElement("td");
-            td6.classList = 'text-center'
-            const dlt_btn = document.createElement('button')
-            dlt_btn.classList = "delete w-6 h-6 mx-auto rounded-sm bg-red-800 text-white text-sm font-semibold";
-            dlt_btn.id = 'delete_item'
-            dlt_btn.innerHTML = "X";
-            td6.appendChild(dlt_btn)
-            
-            full_address.innerHTML = val.address
-            area.innerHTML = val.city
+			const td6 = document.createElement("td");
+			td6.classList = "delete text-center border";
+			const dlt_btn = document.createElement("button");
+			dlt_btn.classList = "delete w-6 h-6 mx-auto rounded-sm bg-red-600 text-white text-sm font-semibold";
+			dlt_btn.id = "delete_item";
+			dlt_btn.innerHTML = "X";
+			td6.appendChild(dlt_btn);
 
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            tr.appendChild(td4);
-            tr.appendChild(td5);
-            tr.appendChild(td6);
+			full_address.innerHTML = val.address;
+			area.innerHTML = val.city;
 
-            show_invoice_data.appendChild(tr);
+			tr.appendChild(td1);
+			tr.appendChild(td2);
+			tr.appendChild(td3);
+			tr.appendChild(td4);
+			tr.appendChild(td5);
+			tr.appendChild(td6);
 
-            your_name.value = "";
-            address.value = "";
-            city.value = "";
-            item_name.value = "";
-            description.value = "";
-            qty.value = "";
-            flat_rate.value = "";
+			show_invoice_data.appendChild(tr);
+
+			your_name.value = "";
+			address.value = "";
+			city.value = "";
+			item_name.value = "";
+			description.value = "";
+			qty.value = "";
+			flat_rate.value = "";
             unit_rate.value = "";
+            
+            
         });
 
-        form_data = [];
+        // window.addEventListener("click", (e) => {
+		// 	e.preventDefault();
 
-        // Total Quantity Count
-        qty_count.push(parseInt(data.qty));
-        let totalQuantity = 0;
+		// 	if (e.target.parentElement.classList.contains("delete")) {
+		// 		e.target.parentElement.parentElement.remove();
+		// 	}
+		// });
 
-        for (let i = 0; i < qty_count.length; i++) {
-            totalQuantity += qty_count[i];
-        }
-        total_qty.innerHTML = totalQuantity;
+		form_data = [];
 
-        // Total Amount Count
-        amount.push(parseInt(data.flat_rate));
-        let totalAmount = 0;
+		// Total Quantity Count
+		qty_count.push(parseInt(data.qty));
+		let totalQuantity = 0;
 
-        for (let i = 0; i < amount.length; i++) {
-            totalAmount += amount[i];
-        }
-        total_amount.forEach((val, key) => {
-            val.innerHTML = totalAmount;
-        })
+		for (let i = 0; i < qty_count.length; i++) {
+			totalQuantity += qty_count[i];
+		}
+		total_qty.innerHTML = totalQuantity;
 
-        // Total Unit Cost Count
-        unit.push(parseInt(data.unit_rate));
-        let totalUnit = 0;
+		// Total Amount Count
+		amount.push(parseInt(data.flat_rate));
+		let totalAmount = 0;
 
-        for (let i = 0; i < unit.length; i++) {
-            totalUnit += unit[i];
-        }
+		for (let i = 0; i < amount.length; i++) {
+			totalAmount += amount[i];
+		}
+		total_amount.forEach((val, key) => {
+			val.innerHTML = totalAmount;
+		});
+
+		// Total Unit Cost Count
+		unit.push(parseInt(data.unit_rate));
+		let totalUnit = 0;
+
+		for (let i = 0; i < unit.length; i++) {
+			totalUnit += unit[i];
+		}
         total_unit.innerHTML = totalUnit;
+        
+        
     }
+    
+    
 })
+
